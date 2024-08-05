4168d03ca57f1b6ba67d92f0837917e2b12776d2
JhilamDas <130777741+JhilamDas@users.noreply.github.com>
2024-08-05 18:32:10 +0530

    Add files via upload

---
+++ Week 2/Week 2 Spring Core Maven/LibraryManagement/src/main/java/com/library/main/MainApp.java
@@ -0,0 +1,18 @@
+package com.library.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.library.service.BookService;
+
+public class MainApp {
+    public static void main(String[] args) {
+        // Load Spring context
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // Retrieve the BookService bean
+        BookService bookService = (BookService) context.getBean("bookService");
+
+        // Call a method on BookService to test injection
+        bookService.performService();
+    }
+}
